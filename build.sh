308096a82270e2d1ee913cbda6cec7e2643b23c3
Peter Lafreniere <peter@n8pjl.ca>
2023-08-25 17:47:59 -0400

    Optimize build switches for size
    We had previously been wasting a few hundred bytes of executable space
    due to missing optimizations, so here we add all compiler + linker flags
    that improve code density. Note that compressed-format rom calls &
    relocations actually increase code size.

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 rm -rf output
 mkdir output/
-tigcc -std=gnu99 -mregparm -Os startup.c opcodes.c sprite.c -o output/ch8ti -Wall -Wextra -DUSE_TI89 --native
-tigcc -std=gnu99 -mregparm -Os startup.c opcodes.c sprite.c -o output/ch8ti -Wall -Wextra -DUSE_TI92P --native
-tigcc -std=gnu99 -mregparm -Os startup.c opcodes.c sprite.c -o output/ch8ti -Wall -Wextra -DUSE_V200 --native
+tigcc -std=gnu99 -mregparm -fno-zero-initialized-in-bss --omit-bss-init \
+ --cut-ranges --reorder-sections --merge-constants -ffunction-sections \
+ -fdata-sections --optimize-code -Os startup.c opcodes.c sprite.c -o \
+ output/ch8ti -Wall -Wextra -DUSE_TI89 -DOPTIMIZE_ROM_CALLS  --native
+
+tigcc -std=gnu99 -mregparm -fno-zero-initialized-in-bss --omit-bss-init \
+ --cut-ranges --reorder-sections --merge-constants -ffunction-sections \
+ -fdata-sections --optimize-code -Os startup.c opcodes.c sprite.c -o \
+ output/ch8ti -Wall -Wextra -DUSE_TI92P -DOPTIMIZE_ROM_CALLS  --native
+
+tigcc -std=gnu99 -mregparm -fno-zero-initialized-in-bss --omit-bss-init \
+ --cut-ranges --reorder-sections --merge-constants -ffunction-sections \
+ -fdata-sections --optimize-code -Os startup.c opcodes.c sprite.c -o \
+ output/ch8ti -Wall -Wextra -DUSE_V200 -DOPTIMIZE_ROM_CALLS  --native
+
 cd preprocessor
 cargo build --release
 # cargo build --target=x86_64-pc-windows-gnu --release
